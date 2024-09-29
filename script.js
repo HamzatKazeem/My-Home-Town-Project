a781273a46dd5ea414830899dc900ade57cd076f
Kazeem Hamzat <hamzatkazeem@yahoo.com>
2024-09-29 02:23:10 +0100

    Update JS file
    Fix rotation direction and remove excess images

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var ImageGallery;
 
-// A Javascript function being used to perform image slider.
+// A Javascript function being to perform image slider.
 function load_gallery() {
   if(--window.LoopTrap <= 0) throw "Infinite loop.";
   let element_image = document.getElementById('image');
@@ -8,7 +8,7 @@ function load_gallery() {
 }
 
 
-ImageGallery = ['https://www.welcometosheffield.co.uk/media/weclorwi/radisson-7-v2-small.jpg?anchor=center&mode=crop&quality=100&width=640&height=320&rnd=133645778875470000', 'https://www.welcometosheffield.co.uk/media/nofbcb2x/cam-street-collective-4.jpg?center=0.58944793850454225,0.50838574423480087&mode=crop&quality=100&width=640&height=320&rnd=133609244536570000', 'https://www.welcometosheffield.co.uk/media/nofbcb2x/cam-street-collective-4.jpg?center=0.58944793850454225,0.50838574423480087&mode=crop&quality=100&width=640&height=320&rnd=133609244536570000', 'https://www.welcometosheffield.co.uk/media/npfbyoja/khyati.jpg?anchor=center&mode=crop&quality=100&width=800&height=640&rnd=132924221403700000', 'https://www.welcometosheffield.co.uk/media/wicp2tle/5dd843b9bfbf2_charles-street-hero.jpg?anchor=center&mode=crop&width=640&height=391&format=webp&quality=100&rnd=132902801678970000'];
+ImageGallery = ['https://www.welcometosheffield.co.uk/media/weclorwi/radisson-7-v2-small.jpg?anchor=center&mode=crop&quality=100&width=640&height=320&rnd=133645778875470000', 'https://www.welcometosheffield.co.uk/media/nofbcb2x/cam-street-collective-4.jpg?center=0.58944793850454225,0.50838574423480087&mode=crop&quality=100&width=640&height=320&rnd=133609244536570000', 'https://www.welcometosheffield.co.uk/media/wicp2tle/5dd843b9bfbf2_charles-street-hero.jpg?anchor=center&mode=crop&width=640&height=391&format=webp&quality=100&rnd=132902801678970000'];
 load_gallery();
 
 
@@ -20,8 +20,8 @@ document.getElementById('nxtBtn').addEventListener('click', (event) => {
 });
 
 document.getElementById('prvBtn').addEventListener('click', (event) => {
-  ImageGallery.push(ImageGallery[0]);
-  ImageGallery.shift();
+  ImageGallery.unshift(ImageGallery.slice(-1)[0]);
+  ImageGallery.pop();
   load_gallery();
 
 });
